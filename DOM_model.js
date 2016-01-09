cd471c84b77bdd8f980dd285653c79bd4673a362
Eugen <Re4port@ukr.net>
2016-01-10 00:45:37 +0200

    added new method Ajax

--- DOM_model.js
+++ DOM_model.js
@@ -4,7 +4,7 @@ var table =  document.getElementById("lol");
 for (var i = table.rows.length-1; i>=0; i--) {
 	var row = table.rows[i];
 	if(i%2 == 0) {
-		for (var j = 0; j < row.cells.length-1; j++) {
+		for (var j = 0; j < row.cells.length; j++) {
 			row.cells[j].style.backgroundColor = "grey";
 		};
 		
@@ -26,3 +26,25 @@ function del(id){
 		alert ("Фух, пронесло..")
 	}*/
 }
+	
+/*нужно попробовать!!!
+function gett(){
+	// 1. Создаём новый объект XMLHttpRequest
+}
+var xhr = new XMLHttpRequest();
+
+// 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+xhr.open('GET', 'style.css', false);
+
+// 3. Отсылаем запрос
+xhr.send();
+
+// 4. Если код ответа сервера не 200, то это ошибка
+if (xhr.status != 200) {
+  // обработать ошибку
+  alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+} else {
+  // вывести результат
+  alert( xhr.responseText ); // responseText -- текст ответа.
+}
+}*/
